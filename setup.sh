1c62a76469423a775a0840de9140a1fd1fa97dab
Neha Kennard <43078984+nnkennard@users.noreply.github.com>
2021-02-08 14:56:42 -0500

    remove redundant corenlp home thing

--- setup.sh
+++ setup.sh
@@ -1,10 +1,3 @@
-#virtualenv --python `which python3` iddve
-#source iddve/bin/activate
-#python -m pip install -r requirements.txt
-
-export CORENLP_HOME="/home/nnayak/stanford-corenlp-full-2018-02-27/"
-# ^ replace with path to your local CoreNLP directory
-
 python3 -m venv iddve
 source iddve/bin/activate
 python -m pip install -r mini_requirements.txt
