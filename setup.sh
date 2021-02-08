07aed162e1484e9325a85c55d0d02cfc98911c92
Neha Kennard <nnayak@cs.umass.edu>
2021-02-08 12:51:19 -0500

    updated with mini requirements

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ export CORENLP_HOME="/home/nnayak/stanford-corenlp-full-2018-02-27/"
 
 python3 -m venv iddve
 source iddve/bin/activate
-python -m pip install -r requirements.txt
+python -m pip install -r mini_requirements.txt
 
 mkdir review_classification
 python build_review_classification.py
